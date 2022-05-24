1f76e7a16081c75d964d0d0437d3e6fec4d21b15
Angel <angloz780@alu.centroafuera.es>
2022-05-24 12:28:59 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/lordOfTheRings/LordOfTheRingsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.lordOfTheRings
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LordOfTheRingsApplication
+
+fun main(args: Array<String>) {
+	runApplication<LordOfTheRingsApplication>(*args)
+}
