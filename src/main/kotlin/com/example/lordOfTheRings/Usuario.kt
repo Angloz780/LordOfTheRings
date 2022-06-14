0cb4875093d77f0454e964e81299f32669c8dfeb
Angel <angloz780@alu.centroafuera.es>
2022-06-14 13:51:30 +0200

    Señor de los anillos arreglar ultimo ejercicio

--- src/main/kotlin/com/example/lordOfTheRings/Usuario.kt
+++ src/main/kotlin/com/example/lordOfTheRings/Usuario.kt
@@ -11,4 +11,27 @@ data class Usuario (@Id var nombre : String, var pass : String) {
 
     @ElementCollection
     var personList = mutableListOf<String>()
+
+    var ganadaFacil: Boolean = false
+    var ganadaMedio: Boolean = false
+    var ganadaDificil: Boolean = false
+
+    var sizeTeam: Int = 6
+    var selecDif = DifDungeon.Facil
+
+    enum class DifDungeon(){
+        Facil,
+        Medio,
+        Dificil
+    }
+
+    fun elegirDif(dif: String){
+        when(dif){
+            "Facil" -> selecDif = DifDungeon.Facil
+            "Medio" -> selecDif = DifDungeon.Medio
+            "Dificil" -> selecDif = DifDungeon.Dificil
+            else ->
+                println("Nivel de dificultad mal escrita")
+        }
+    }
 }
--- src/main/kotlin/com/example/lordOfTheRings/UsuarioController.kt
+++ src/main/kotlin/com/example/lordOfTheRings/UsuarioController.kt
@@ -1,9 +1,11 @@
 package com.example.lordOfTheRings
 
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import kotlin.random.Random
 
 @RestController
 class UsuarioController (private  val usuarioRepository: UsuarioRepository) {
@@ -62,6 +64,152 @@ class UsuarioController (private  val usuarioRepository: UsuarioRepository) {
                     }
                 }
                 return "El personaje no pertence al equipo"
+            }
+        }
+        return "Token no valido"
+    }
+
+    @GetMapping("adentrarnseEnMazmorra/{token}/{dificultad}")
+    fun adentrarnseEnMazmorra(@PathVariable token: String, @PathVariable dificultad: String): Any{
+
+        var prob: Int
+        var lista = mutableListOf<Doc>()
+
+        usuarioRepository.findAll().forEach{ usuario ->
+            if(usuario.token == token){
+                if (usuario.personList.size > 1){
+                    usuario.elegirDif(dificultad)
+                    when(usuario.selecDif){
+                        Usuario.DifDungeon.Facil ->{
+                            if (usuario.ganadaFacil)
+                                return "Dungeon ya superada"
+                            else{
+                                for (i in 1 .. usuario.personList.size){
+                                    prob = Random.nextInt(1, 6)
+                                    if (prob == 1){
+                                        usuarioRepository.findAll().forEach { usuario ->
+                                            if (usuario.token == token){
+                                                usuario.personList.forEach {
+                                                    characterList.docs.forEach { personaje ->
+                                                        if (it == personaje.id)
+                                                            personaje.seleccion = false
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                if (usuario.personList.size < 1)
+                                    return "Equipo muerto"
+                                else{
+                                    usuario.ganadaFacil = true
+
+                                    usuarioRepository.findAll().forEach { usuario ->
+                                        if (usuario.token == token){
+                                            usuario.personList.forEach {
+                                                characterList.docs.forEach { personaje ->
+                                                    if(it == personaje.id)
+                                                        lista.add(personaje)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (usuario.ganadaMedio && usuario.ganadaDificil){
+                                        usuario.sizeTeam = usuario.sizeTeam + 1
+                                        asignarEquipo(token)
+                                    }
+                                    return "Has ganado la dungeon de nivel Facil $lista"
+                                }
+                            }
+                        }
+                        Usuario.DifDungeon.Medio ->{
+                            if (usuario.ganadaMedio)
+                                return "Dungeon ya superada"
+                            else{
+                                for (i in 1 .. usuario.personList.size){
+                                    prob = Random.nextInt(1, 2)
+                                    if(prob == 1){
+                                        usuarioRepository.findAll().forEach { usuario ->
+                                            if (usuario.token == token){
+                                                usuario.personList.forEach {
+                                                    characterList.docs.forEach { personaje ->
+                                                        if (it == personaje.id)
+                                                            personaje.seleccion = false
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                if (usuario.personList.size < 1)
+                                    return "Equipo muerto"
+                                else{
+                                    usuario.ganadaMedio = true
+
+                                    usuarioRepository.findAll().forEach { usuario ->
+                                        if(usuario.token == token){
+                                            usuario.personList.forEach {
+                                                characterList.docs.forEach { personaje ->
+                                                    if (it == personaje.id)
+                                                        lista.add(personaje)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (usuario.ganadaFacil && usuario.ganadaDificil){
+                                        usuario.sizeTeam = usuario.sizeTeam + 1
+                                        asignarEquipo(token)
+                                    }
+                                    return "Has ganado la dungeon de nivel Medio $lista"
+                                }
+                            }
+                        }
+                        Usuario.DifDungeon.Dificil ->{
+                            if (usuario.ganadaDificil)
+                                return "Dungeon superada"
+                            else{
+                                for(i in 1 .. usuario.personList.size){
+                                    prob = Random.nextInt(1, 4)
+                                    if (prob == 1 || prob == 2 || prob == 3){
+                                        usuarioRepository.findAll().forEach { usuario ->
+                                            if (usuario.token == token){
+                                                usuario.personList.forEach {
+                                                    characterList.docs.forEach { personaje ->
+                                                        if (it == personaje.id)
+                                                            personaje.seleccion = false
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                if (usuario.personList.size < 1)
+                                    return "Equipo muerto"
+                                else{
+                                    usuario.ganadaDificil = true
+
+                                    usuarioRepository.findAll().forEach { usuario ->
+                                        if (usuario.token == token){
+                                            usuario.personList.forEach {
+                                                characterList.docs.forEach { personaje ->
+                                                    if(it == personaje.id)
+                                                        lista.add(personaje)
+                                                }
+                                            }
+                                        }
+                                    }
+                                    if (usuario.ganadaFacil && usuario.ganadaMedio){
+                                        usuario.sizeTeam = usuario.sizeTeam + 1
+                                        asignarEquipo(token)
+                                    }
+                                    return "Has ganado la dungeon de nivel Dificil $lista"
+                                }
+                            }
+                        }
+                    }
+                }else{
+                    return "Equipo muerto"
+                }
             }
         }
         return "Token no valido"
